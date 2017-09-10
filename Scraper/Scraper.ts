0dde0fd7483f4e98ee22a2c54e2649ea435bc06d
tyler-whited <tyler.whited@domo.com>
2017-09-10 17:28:48 -0600

    Differentiate headers from data in scraper. Added node_modules to gitignore.

--- Scraper/Scraper.ts
+++ Scraper/Scraper.ts
@@ -1,10 +1,14 @@
+const fs = require('fs');
+
 let cheerio = require('cheerio');
 
 import cacheLoader from './CacheLoader';
 
+const HEADER_ROW_IDX:number = 2;
+
 export default class Scraper{
 
-  private $;
+  rows:any;
 
   constructor(){
 
@@ -12,7 +16,56 @@ export default class Scraper{
 
   getParsed():Promise<any>{
     return cacheLoader().then(res=>{
-      let $ = cheerio.load('res');
-    })
+      let $ = cheerio.load(res);
+      let foundRows = $($(`div.main table`).get(0)).find('tr');
+      this.rows = [];
+      for(var i = 0; i < foundRows.length; ++i){
+        this.rows.push(parseFoundRow(foundRows[i].children));
+      }
+      let headerRow = this.rows[HEADER_ROW_IDX];
+        // .filter(cell=>cell.children)
+        // .map(cell=>return cell.children[0].children[0].data});
+      // fs.writeFile('./json_data.json', JSON.stringify(this.rows), err=>{
+      //   if(err) throw err;
+      // });
+      console.log(headerRow);
+      console.log('firstFoundRow:');
+      console.log(parseFoundRow(this.rows[HEADER_ROW_IDX+1]));
+    });
   }
+
+  
+}
+
+//pure
+function parseFoundRow(foundRow):string[]{
+  let result;
+  foundRow = foundRow.filter(cell=>cell.type==='tag');
+  if(foundRow[0].name === 'th'){
+    console.log(foundRow[0]);
+    result = parseHeaderRow(foundRow);
+  }else{
+    result = parseContentRow(foundRow);
+  }
+  return result;
+    
+}
+
+function parseHeaderRow(row){
+  return row
+    .filter(cell=>cell.children)
+    .map(cell=>cell.children[0].children[0].data);
+}
+
+function parseContentRow(row){
+  return row.map(cell=>{
+    let data;
+    cell = cell.children[0];
+    if(cell.name === 'img'){
+      data = cell.attribs.src;
+    }else{
+      data = cell.data
+    }
+    return data;
+  });
 }
---
+++ emoji/Emoji.ts
@@ -0,0 +1,14 @@
+// Stateful emoji data structure, categrozied
+
+export default class Emoji{
+  private currentCategory;
+  
+
+  constructor(){
+
+  }
+
+  nextCategory(name:string){
+
+  }
+}
